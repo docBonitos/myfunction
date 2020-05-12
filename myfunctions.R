b06863591738af58daa13eb3277fbf46b0377b5b
docBonitos <bonifacio.pasion@gmail.com>
2020-05-12 18:36:32 +0800

    First function for regression model shortcut

---
+++ myfunctions.R
@@ -0,0 +1,25 @@
+#regression model to multiple columns and subsections
+fit_fun <- function(df){
+  summary(lm(y_var ~ x_var, data = df)) #depending on the data
+}
+by(df, df$group, fit_fun)
+
+
+fit_fun <- function(df, y){ #for changing y variables
+  lm_mod <- substitute(
+    y ~ Census_Interval,
+    list(y = substitute(y))
+  )
+  eval(summary(lm(lm_mod, data = df)))
+}
+by(df, y = y_var, df$group, fit_fun)
+
+
+fit_fun <- function(df, y){ #for changing y and x variables
+  lm_mod <- substitute(
+    y ~ x,
+  list(y = substitute(y)), x = substitute(x))
+  )
+eval(summary(lm(lm_mod, data = df)))
+}
+by(df, y = y_var, x = x_var, df$group, fit_fun)
